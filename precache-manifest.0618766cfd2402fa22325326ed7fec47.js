6d3787e83dc552bf5b49bddaf5967e323c2db848
unknown <shihyihong@gmail.com>
2019-07-28 14:49:10 +0800

    tetetetetetete

--- precache-manifest.0618766cfd2402fa22325326ed7fec47.js
+++ precache-manifest.0618766cfd2402fa22325326ed7fec47.js
@@ -1,27 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "425b35d0bdccd3348f95d65c4371ce6e",
+    "revision": "23f33564ae72e8efe83863e02632f0c2",
     "url": "/index.html"
   },
   {
-    "revision": "6ba44fd5259c014cf249",
-    "url": "/static/css/main.029219b0.chunk.css"
+    "revision": "fdcbddf14d0e1dc2f6df",
+    "url": "/static/css/main.e7ab9064.chunk.css"
   },
   {
     "revision": "d92194327d9779a1b6b9",
     "url": "/static/js/2.39e966be.chunk.js"
   },
   {
-    "revision": "6ba44fd5259c014cf249",
-    "url": "/static/js/main.c9f4cf8a.chunk.js"
+    "revision": "fdcbddf14d0e1dc2f6df",
+    "url": "/static/js/main.7d19c43c.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
-  },
-  {
-    "revision": "1b69ac767a6153ded3cd0c48a4cc8d59",
-    "url": "/static/media/IG.1b69ac76.jpg"
   },
   {
     "revision": "534f24a72afa0bc8a8313e8174f6f19b",
@@ -48,8 +44,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/background1.9976b74f.png"
   },
   {
-    "revision": "324b2215d87d6fc233e7955ed8aa7589",
-    "url": "/static/media/linkedin.324b2215.jpg"
+    "revision": "fae19e0920e6e41a2b0f2eeaec804647",
+    "url": "/static/media/ig.fae19e09.png"
   },
   {
     "revision": "6042cac21160f13286903f21a79396b7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dbd514362dce7ddbe7f4b0a93a29b852.js"
+  "/precache-manifest.0618766cfd2402fa22325326ed7fec47.js"
 );
 
 self.addEventListener('message', (event) => {
